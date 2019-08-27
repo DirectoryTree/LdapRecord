891b528d40c8930d562add5968c827e724357ee2
Steve Bauman <steven_bauman@outlook.com>
2019-08-27 17:14:22 -0400

    Fixed connection tests

--- tests/Connections/ConnectionTest.php
+++ tests/Connections/ConnectionTest.php
@@ -3,48 +3,43 @@
 namespace LdapRecord\Tests\Connections;
 
 use LdapRecord\Tests\TestCase;
+use LdapRecord\Auth\BindException;
 use LdapRecord\Connections\Connection;
 use LdapRecord\Connections\LdapInterface;
 use LdapRecord\Connections\DetailedError;
+use LdapRecord\Auth\UsernameRequiredException;
+use LdapRecord\Auth\PasswordRequiredException;
 use LdapRecord\Configuration\DomainConfiguration;
 
 class ConnectionTest extends TestCase
 {
-    protected function newConnection($configuration = [])
-    {
-        return new Connection($configuration);
-    }
-
     public function test_construct()
     {
-        $m = $this->newConnection(new DomainConfiguration());
+        $conn = new Connection(new DomainConfiguration());
 
-        $this->assertInstanceOf(LdapInterface::class, $m->getLdapConnection());
-        $this->assertInstanceOf(DomainConfiguration::class, $m->getConfiguration());
+        $this->assertInstanceOf(LdapInterface::class, $conn->getLdapConnection());
+        $this->assertInstanceOf(DomainConfiguration::class, $conn->getConfiguration());
     }
 
     public function test_auth_username_failure()
     {
-        $this->expectException(\LdapRecord\Auth\UsernameRequiredException::class);
+        $ldap = $this->mock(LdapInterface::class);
 
-        $ldap = $this->newLdapMock();
+        $ldap->shouldReceive('setOptions')->once();
+        $ldap->shouldReceive('connect')->once();
+        $ldap->shouldReceive('isBound')->once()->andReturn(true);
+        $ldap->shouldReceive('close')->once()->andReturn(true);
 
-        $ldap
-            ->shouldReceive('setOptions')->once()
-            ->shouldReceive('connect')->once()
-            ->shouldReceive('isBound')->once()->andReturn(true)
-            ->shouldReceive('close')->once()->andReturn(true);
+        $conn = new Connection();
+        $conn->setLdapConnection($ldap);
 
-        $c = $this->newConnection();
-
-        $c->setLdapConnection($ldap);
-
-        $c->auth()->attempt(0000000, 'password');
+        $this->expectException(UsernameRequiredException::class);
+        $conn->auth()->attempt(0000000, 'password');
     }
 
     public function test_auth_password_failure()
     {
-        $this->expectException(\LdapRecord\Auth\PasswordRequiredException::class);
+        $this->expectException(PasswordRequiredException::class);
 
         $ldap = $this->newLdapMock();
 
@@ -63,21 +58,20 @@ class ConnectionTest extends TestCase
 
     public function test_auth_failure()
     {
-        $ldap = $this->newLdapMock();
+        $ldap = $this->mock(LdapInterface::class);
 
         // Binding as the user.
-        $ldap
-            ->shouldReceive('connect')->once()->andReturn(true)
-            ->shouldReceive('setOptions')->once()
-            ->shouldReceive('bind')->once()->withArgs(['username', 'password'])->andReturn(false);
+        $ldap->shouldReceive('connect')->once()->andReturn(true);
+        $ldap->shouldReceive('setOptions')->once();
+        $ldap->shouldReceive('bind')->once()->withArgs(['username', 'password'])->andReturn(false);
 
         $error = new DetailedError(42, 'Invalid credentials', '80090308: LdapErr: DSID-0C09042A');
 
         // Binding fails, retrieves last error.
-        $ldap->shouldReceive('getLastError')->once()->andReturn('error')
-            ->shouldReceive('getDetailedError')->once()->andReturn($error)
-            ->shouldReceive('isBound')->once()->andReturn(true)
-            ->shouldReceive('errNo')->once()->andReturn(1);
+        $ldap->shouldReceive('getLastError')->once()->andReturn('error');
+        $ldap->shouldReceive('getDetailedError')->once()->andReturn($error);
+        $ldap->shouldReceive('isBound')->once()->andReturn(true);
+        $ldap->shouldReceive('errNo')->once()->andReturn(1);
 
         // Rebinds as the administrator.
         $ldap->shouldReceive('bind')->once()->withArgs([null, null])->andReturn(true);
@@ -85,79 +79,70 @@ class ConnectionTest extends TestCase
         // Closes the connection.
         $ldap->shouldReceive('close')->once()->andReturn(true);
 
-        $c = $this->newConnection();
+        $conn = new Connection();
+        $conn->setLdapConnection($ldap);
 
-        $c->setLdapConnection($ldap);
-
-        $this->assertFalse($c->auth()->attempt('username', 'password'));
+        $this->assertFalse($conn->auth()->attempt('username', 'password'));
     }
 
     public function test_auth_passes_with_rebind()
     {
         $config = new DomainConfiguration([
-            'username' => 'test',
-            'password' => 'test',
+            'username' => 'foo',
+            'password' => 'bar',
         ]);
 
-        $ldap = $this->newLdapMock();
+        $ldap = $this->mock(LdapInterface::class);
 
-        $ldap
-            ->shouldReceive('connect')->once()->andReturn(true)
-            ->shouldReceive('setOptions')->once()
-            ->shouldReceive('isUsingSSL')->once()->andReturn(false)
-            ->shouldReceive('isBound')->once()->andReturn(true);
+        $ldap->shouldReceive('connect')->once()->andReturn(true);
+        $ldap->shouldReceive('setOptions')->once();
+        $ldap->shouldReceive('isBound')->once()->andReturn(true);
 
         // Authenticates as the user
         $ldap->shouldReceive('bind')->once()->withArgs(['username', 'password'])->andReturn(true);
 
         // Re-binds as the administrator
-        $ldap
-            ->shouldReceive('bind')->once()->withArgs(['test', 'test'])->andReturn(true)
-            ->shouldReceive('isBound')->once()->andReturn(true)
-            ->shouldReceive('close')->once()->andReturn(true);
+        $ldap->shouldReceive('bind')->once()->withArgs(['foo', 'bar'])->andReturn(true);
+        $ldap->shouldReceive('close')->once()->andReturn(true);
 
-        $c = $this->newConnection($config);
+        $conn = new Connection($config);
+        $conn->setLdapConnection($ldap);
 
-        $c->setLdapConnection($ldap);
-
-        $this->assertTrue($c->auth()->attempt('username', 'password'));
+        $this->assertTrue($conn->auth()->attempt('username', 'password'));
     }
 
     public function test_auth_rebind_failure()
     {
-        $this->expectException(\LdapRecord\Auth\BindException::class);
-
         $config = new DomainConfiguration([
             'username' => 'test',
             'password' => 'test',
         ]);
 
-        $ldap = $this->newLdapMock();
+        $ldap = $this->mock(LdapInterface::class);
 
-        $ldap
-            ->shouldReceive('connect')->once()->andReturn(true)
-            ->shouldReceive('setOptions')->once()
-            ->shouldReceive('isUsingSSL')->once()->andReturn(false)
-            ->shouldReceive('isBound')->once()->andReturn(true);
+        $ldap->shouldReceive('connect')->once()->andReturn(true);
+        $ldap->shouldReceive('setOptions')->once();
+        $ldap->shouldReceive('isUsingSSL')->once()->andReturn(false);
+        $ldap->shouldReceive('isBound')->once()->andReturn(true);
 
         // Authenticates as the user
         $ldap->shouldReceive('bind')->once()->withArgs(['username', 'password']);
 
         // Re-binds as the administrator (fails)
-        $ldap->shouldReceive('bind')->once()->withArgs(['test', 'test'])->andReturn(false)
-            ->shouldReceive('getLastError')->once()->andReturn('')
-            ->shouldReceive('getDetailedError')->once()->andReturn(new DetailedError(null, null, null))
-            ->shouldReceive('isBound')->once()->andReturn(true)
-            ->shouldReceive('errNo')->once()->andReturn(1)
-            ->shouldReceive('close')->once()->andReturn(true);
+        $ldap->shouldReceive('bind')->once()->withArgs(['test', 'test'])->andReturn(false);
+        $ldap->shouldReceive('getLastError')->once()->andReturn('');
+        $ldap->shouldReceive('getDetailedError')->once()->andReturn(new DetailedError(null, null, null));
+        $ldap->shouldReceive('isBound')->once()->andReturn(true);
+        $ldap->shouldReceive('errNo')->once()->andReturn(1);
+        $ldap->shouldReceive('close')->once()->andReturn(true);
 
-        $c = $this->newConnection($config);
+        $this->expectException(BindException::class);
 
-        $c->setLdapConnection($ldap);
+        $conn = new Connection($config);
+        $conn->setLdapConnection($ldap);
+        $conn->connect();
 
-        $c->connect();
-
-        $this->assertTrue($c->auth()->attempt('username', 'password'));
+        $this->assertTrue($conn->auth()->attempt('username', 'password'));
     }
 
     public function test_auth_passes_without_rebind()

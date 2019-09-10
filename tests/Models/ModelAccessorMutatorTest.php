7a5594705522587e58745c77910d6ed881f7a745
Steve Bauman <steven_bauman@outlook.com>
2019-09-10 18:25:43 -0400

    Added more tests

--- tests/Models/ModelAccessorMutatorTest.php
+++ tests/Models/ModelAccessorMutatorTest.php
@@ -57,7 +57,7 @@ class ModelAccessorTest extends TestCase
         $this->assertNull($model->getAttribute('foobar'));
     }
 
-    public function test_models_convert_dates_to_ldap_type()
+    public function test_models_mutate_dates_to_ldap_type()
     {
         $model = new ModelDateMutatorStub();
         $date = new Carbon();
@@ -70,7 +70,7 @@ class ModelAccessorTest extends TestCase
         $this->assertEquals($date->format('YmdHis\Z'), $model->getAttributes()['createtimestamp'][0]);
     }
 
-    public function test_models_convert_dates_to_windows_type()
+    public function test_models_mutate_dates_to_windows_type()
     {
         $model = new ModelDateMutatorStub();
         $date = new Carbon();
@@ -83,7 +83,7 @@ class ModelAccessorTest extends TestCase
         $this->assertEquals($date->format('YmdHis.0\Z'), $model->getAttributes()['whenchanged'][0]);
     }
 
-    public function test_models_convert_dates_to_windows_integer_type()
+    public function test_models_mutate_dates_to_windows_integer_type()
     {
         $model = new ModelDateMutatorStub();
         $date = new Carbon();
@@ -95,12 +95,48 @@ class ModelAccessorTest extends TestCase
         $this->assertEquals($date->setTimezone('UTC')->micro(0), $model->accountexpires);
         $this->assertEquals(Utilities::convertUnixTimeToWindowsTime($date->getTimestamp()), $model->getAttributes()['accountexpires'][0]);
     }
+
+    public function test_models_mutate_from_ldap_type_to_date()
+    {
+        $model = new ModelDateAccessorStub();
+
+        $this->assertInstanceOf(Carbon::class, $model->createtimestamp);
+        $this->assertInstanceOf(Carbon::class, $model->CreateTimestamp);
+        $this->assertEquals('2019-09-10 22:02:04', $model->createtimestamp->toDateTimeString());
+
+        $model->createTimestamp = $model->createTimestamp->addMinute();
+        $this->assertEquals('2019-09-10 22:03:04', $model->createTimestamp->toDateTimeString());
+    }
+
+    public function test_models_mutate_from_windows_type_to_date()
+    {
+        $model = new ModelDateAccessorStub();
+
+        $this->assertInstanceOf(Carbon::class, $model->whenchanged);
+        $this->assertInstanceOf(Carbon::class, $model->WhenChanged);
+        $this->assertEquals('2019-09-10 22:02:04', $model->whenchanged->toDateTimeString());
+
+        $model->whenChanged = $model->whenChanged->addMinute();
+        $this->assertEquals('2019-09-10 22:03:04', $model->whenchanged->toDateTimeString());
+    }
+
+    public function test_models_mutate_from_windows_integer_type_to_date()
+    {
+        $model = new ModelDateAccessorStub();
+
+        $this->assertInstanceOf(Carbon::class, $model->accountexpires);
+        $this->assertInstanceOf(Carbon::class, $model->AccountExpires);
+        $this->assertEquals('2019-09-10 22:02:04', $model->accountexpires->toDateTimeString());
+
+        $model->accountExpires = $model->accountExpires->addMinute();
+        $this->assertEquals('2019-09-10 22:03:04', $model->accountExpires->toDateTimeString());
+    }
 }
 
 class ModelAccessorStub extends Model
 {
     protected $attributes = [
-        'foo'     => ['bar'],
+        'foo' => ['bar'],
         'foo-bar' => ['baz'],
     ];
 
@@ -118,7 +154,7 @@ class ModelAccessorStub extends Model
 class ModelMutatorStub extends Model
 {
     protected $attributes = [
-        'foo'     => ['bar'],
+        'foo' => ['bar'],
         'foo-bar' => ['baz'],
     ];
 
@@ -137,7 +173,16 @@ class ModelDateMutatorStub extends Model
 {
     protected $dates = [
         'createTimestamp' => 'ldap',
-        'whenchanged'     => 'windows',
-        'accountexpires'  => 'windows-int',
+        'whenchanged' => 'windows',
+        'accountexpires' => 'windows-int'
+    ];
+}
+
+class ModelDateAccessorStub extends ModelDateMutatorStub
+{
+    protected $attributes = [
+        'createTimestamp' => ['20190910220204Z'],
+        'whenchanged' => ['20190910220204.0Z'],
+        'accountexpires' => ['132126265240000000'],
     ];
 }

4598811442fd1fba27ecfb483273b4230dcdfbca
Steve Bauman <steven_bauman@outlook.com>
2019-09-13 10:00:32 -0400

    Fixed tests

--- tests/Query/Model/ActiveDirectoryTest.php
+++ tests/Query/Model/ActiveDirectoryTest.php
@@ -17,7 +17,21 @@ class ActiveDirectoryTest extends TestCase
     {
         $b = $this->newBuilder();
 
-        $b->whereMemberOf('cn=Accounting,dc=org,dc=acme');
+        $b->whereMemberOf('cn=Accounting,dc=org,dc=acme', $nested = false);
+
+        $where = $b->filters['and'][0];
+
+        $this->assertEquals('memberof', $where['field']);
+        $this->assertEquals('=', $where['operator']);
+        $this->assertEquals('\63\6e\3d\41\63\63\6f\75\6e\74\69\6e\67\2c\64\63\3d\6f\72\67\2c\64\63\3d\61\63\6d\65', $where['value']);
+        $this->assertEquals('(memberof=cn=Accounting,dc=org,dc=acme)', $b->getUnescapedQuery());
+    }
+
+    public function test_where_member_of_nested()
+    {
+        $b = $this->newBuilder();
+
+        $b->whereMemberOf('cn=Accounting,dc=org,dc=acme', $nested = true);
 
         $where = $b->filters['and'][0];
 
@@ -32,7 +46,25 @@ class ActiveDirectoryTest extends TestCase
         $b = $this->newBuilder();
 
         $b->orWhereEquals('cn', 'John Doe');
-        $b->orWhereMemberOf('cn=Accounting,dc=org,dc=acme');
+        $b->orWhereMemberOf('cn=Accounting,dc=org,dc=acme', $nested = false);
+
+        $where = $b->filters['or'][1];
+
+        $this->assertEquals('memberof', $where['field']);
+        $this->assertEquals('=', $where['operator']);
+        $this->assertEquals('\63\6e\3d\41\63\63\6f\75\6e\74\69\6e\67\2c\64\63\3d\6f\72\67\2c\64\63\3d\61\63\6d\65', $where['value']);
+        $this->assertEquals(
+            '(|(cn=John Doe)(memberof=cn=Accounting,dc=org,dc=acme))',
+            $b->getUnescapedQuery()
+        );
+    }
+
+    public function test_or_where_member_of_nested()
+    {
+        $b = $this->newBuilder();
+
+        $b->orWhereEquals('cn', 'John Doe');
+        $b->orWhereMemberOf('cn=Accounting,dc=org,dc=acme', $nested = true);
 
         $where = $b->filters['or'][1];
 

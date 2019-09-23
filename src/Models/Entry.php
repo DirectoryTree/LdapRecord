054ca02175d817e4bdeb3056561f4f8e2e0f0797
Steve Bauman <steven_bauman@outlook.com>
2019-09-23 11:13:08 -0400

    Added query scopes for descedants and ancestors

--- src/Models/Entry.php
+++ src/Models/Entry.php
@@ -4,5 +4,23 @@ namespace LdapRecord\Models;
 
 class Entry extends Model
 {
-    //
+    /**
+     * Begin querying the direct descendants of the model.
+     *
+     * @return \LdapRecord\Query\Model\Builder
+     */
+    public function descendants()
+    {
+        return $this->in($this->getDn())->listing();
+    }
+
+    /**
+     * Begin querying the direct ancestors of the model.
+     *
+     * @return \LdapRecord\Query\Model\Builder
+     */
+    public function ancestors()
+    {
+        return $this->in($this->getParentDn($this->getDn()))->listing();
+    }
 }

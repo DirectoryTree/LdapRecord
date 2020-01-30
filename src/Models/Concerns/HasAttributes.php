e40c63e586763e3341f84a48ba7d14996785ec71
Steve Bauman <steven_bauman@outlook.com>
2020-01-30 12:04:19 -0500

    Added clearer code doc.

--- src/Models/Concerns/HasAttributes.php
+++ src/Models/Concerns/HasAttributes.php
@@ -208,6 +208,10 @@ trait HasAttributes
     /**
      * Determine if the given value is an LDAP reset integer.
      *
+     * The integer values '0' and '-1' can be used on certain
+     * LDAP attributes to instruct the server to reset the
+     * value to an 'unset' or 'cleared' state.
+     *
      * @param mixed $value
      *
      * @return bool

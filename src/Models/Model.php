d0dbb83963ba373c2f4adf088e316d9cd91aacb0
Steve Bauman <steven_bauman@outlook.com>
2020-02-21 13:28:11 -0500

    Update Model.php

--- src/Models/Model.php
+++ src/Models/Model.php
@@ -689,7 +689,7 @@ abstract class Model implements ArrayAccess, JsonSerializable
     }
 
     /**
-     * Set the models modifications array.
+     * Set the models batch modifications.
      *
      * @param array $modifications
      *
@@ -707,7 +707,7 @@ abstract class Model implements ArrayAccess, JsonSerializable
     }
 
     /**
-     * Adds a batch modification to the models modifications array.
+     * Adds a batch modification to the model.
      *
      * @param array|BatchModification $mod
      *

5ee183477e831ac59a445410e5638a890069af29
Steve Bauman <steven_bauman@outlook.com>
2020-04-02 12:26:48 -0400

    Accept DateIntervals and convert all dates to seconds
    #99

--- src/Query/Cache.php
+++ src/Query/Cache.php
@@ -3,6 +3,9 @@
 namespace LdapRecord\Query;
 
 use Closure;
+use Carbon\Carbon;
+use DateInterval;
+use DateTimeInterface;
 use Psr\SimpleCache\CacheInterface;
 
 class Cache
@@ -43,7 +46,7 @@ class Cache
      *
      * @param string                                    $key
      * @param mixed                                     $value
-     * @param \DateTimeInterface|\DateInterval|int|null $ttl
+     * @param DateTimeInterface|DateInterval|int|null $ttl
      *
      * @throws \Psr\SimpleCache\InvalidArgumentException
      *
@@ -51,15 +54,15 @@ class Cache
      */
     public function put($key, $value, $ttl = null)
     {
-        return $this->store->set($key, $value, $ttl);
+        return $this->store->set($key, $value, $this->getSeconds($ttl));
     }
 
     /**
      * Get an item from the cache, or execute the given Closure and store the result.
      *
-     * @param string                                    $key
-     * @param \DateTimeInterface|\DateInterval|int|null $ttl
-     * @param Closure                                   $callback
+     * @param string                                  $key
+     * @param DateTimeInterface|DateInterval|int|null $ttl
+     * @param Closure                                 $callback
      *
      * @throws \Psr\SimpleCache\InvalidArgumentException
      *
@@ -73,7 +76,7 @@ class Cache
             return $value;
         }
 
-        $this->put($key, $value = $callback(), $ttl);
+        $this->put($key, $value = $callback(), $this->getSeconds($ttl));
 
         return $value;
     }
@@ -83,7 +86,6 @@ class Cache
      *
      * @param string $key
      *
-     * @throws \Psr\Cache\InvalidArgumentException
      * @throws \Psr\SimpleCache\InvalidArgumentException
      *
      * @return bool
@@ -91,5 +93,43 @@ class Cache
     public function delete($key)
     {
         return $this->store->delete($key);
+    }
+
+    /**
+     * Calculate the number of seconds for the given TTL.
+     *
+     * @author Taylor Otwell
+     *
+     * @param DateTimeInterface|DateInterval|int $ttl
+     *
+     * @return int
+     */
+    protected function getSeconds($ttl)
+    {
+        $duration = $this->parseDateInterval($ttl);
+
+        if ($duration instanceof DateTimeInterface) {
+            $duration = Carbon::now()->diffInRealSeconds($duration, false);
+        }
+
+        return (int) $duration > 0 ? $duration : 0;
+    }
+
+    /**
+     * If the given value is an interval, convert it to a DateTime instance.
+     *
+     * @author Taylor Otwell
+     *
+     * @param DateTimeInterface|DateInterval|int $delay
+     *
+     * @return DateTimeInterface|int
+     */
+    protected function parseDateInterval($delay)
+    {
+        if ($delay instanceof DateInterval) {
+            $delay = Carbon::now()->add($delay);
+        }
+
+        return $delay;
     }
 }

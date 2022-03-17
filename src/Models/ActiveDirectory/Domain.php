ac365de1e43fcb1649ce43910882a913518c9ac1
dkuzevanov <kJfFNY43QxbpUqNjxfhQ>
2022-03-17 16:11:36 +0300

    Revert "Fix Scrutinizer checks"
    This reverts commit 5e6c803bebbc7a642b5d4066baf24dcf4ce8ae9d.

--- src/Models/ActiveDirectory/Domain.php
+++ src/Models/ActiveDirectory/Domain.php
@@ -16,15 +16,15 @@ class Domain extends Entry
     ];
 
     /**
-     * @return int in seconds
+     * @return int
      */
     public static function getLockoutDuration(): int
     {
-        $rootDSE = Entry::query()->find('')->first(['defaultNamingContext']);
+        $rootDSE = Entry::query()->find('');
         $base = $rootDSE->getFirstAttribute('defaultNamingContext');
-        $domain = static::query()->find($base)->first(['lockoutDuration']);
-        $duration = (int) $domain->getFirstAttribute('lockoutDuration');
+        $domain = static::query()->find($base);
+        $duration = $domain->getFirstAttribute('lockoutDuration');
 
-        return -1 * (int) round($duration / 10000000);
+        return -1 * round((int) $duration / 10000000);
     }
 }

c0d7446167ecfd9b36e2c8e2b88b86d30ddbd100
Steve Bauman <steven_bauman@outlook.com>
2022-04-15 13:53:02 -0400

    Add model serialization test

---
+++ tests/Models/ModelSerializationTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace LdapRecord\Tests\Models;
+
+use LdapRecord\Models\Attributes\Guid;
+use LdapRecord\Models\ActiveDirectory\Entry;
+use LdapRecord\Models\Attributes\Sid;
+use LdapRecord\Tests\TestCase;
+
+class ModelSerializationTest extends TestCase
+{
+    public function testModelWithBinaryGuidAndSidCanBeSerializedAndEncoded()
+    {
+        $guid = new Guid('2bba564a-4f95-4cb0-97b0-94c0e3458621');
+        $sid = new Sid('S-1-5-21-1004336348-1177238915-682003330-512');
+
+        $model = (new Entry())->setRawAttributes([
+            'objectguid' => [$guid->getBinary()],
+            'objectsid' => [$sid->getBinary()],
+        ]);
+
+        $encodedAndSerialized = json_encode(serialize($model));
+
+        $this->assertIsString($encodedAndSerialized);
+
+        $unserializedAndUnencoded = unserialize(json_decode($encodedAndSerialized));
+
+        $this->assertInstanceOf(Entry::class, $unserializedAndUnencoded);
+
+        $this->assertTrue($model->is($unserializedAndUnencoded));
+
+        $this->assertEquals($model->getConvertedGuid(), $unserializedAndUnencoded->getConvertedGuid());
+        $this->assertEquals($model->getConvertedSid(), $unserializedAndUnencoded->getConvertedSid());
+
+        $this->assertNotEquals($model->getAttributes()['objectguid'], $unserializedAndUnencoded->getAttributes()['objectguid']);
+        $this->assertNotEquals($model->getAttributes()['objectsid'], $unserializedAndUnencoded->getAttributes()['objectsid']);
+    }
+}

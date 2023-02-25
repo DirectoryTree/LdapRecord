6070f35584ccc3192107bb0d98ace1c27f56f3be
Jens Svalgaard Kohrt <github@svalgaard.net>
2023-02-25 17:43:09 +0100

    Fix style

--- src/Query/Builder.php
+++ src/Query/Builder.php
@@ -774,7 +774,7 @@ class Builder
     protected function getCacheKey($query)
     {
         $host = $this->connection->getLdapConnection()->getHost();
-        if (!$host) {
+        if (! $host) {
             $this->connection->connect();
             $host = $this->connection->getLdapConnection()->getHost();
         }

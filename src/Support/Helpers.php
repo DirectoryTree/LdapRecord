791a15c0c583849b85518d6127c05001155dc594
Steve Bauman <steven_bauman@outlook.com>
2023-04-23 21:57:20 -0400

    Add missing return type

--- src/Support/Helpers.php
+++ src/Support/Helpers.php
@@ -9,7 +9,7 @@ class Helpers
     /**
      * Return the default value of the given value.
      */
-    public static function value(mixed $value, mixed ...$args)
+    public static function value(mixed $value, mixed ...$args): mixed
     {
         return $value instanceof Closure ? $value(...$args) : $value;
     }

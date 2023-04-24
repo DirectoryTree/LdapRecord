3f64317761834c0a124c97f1666bda2f1e08bd27
Steve Bauman <steven_bauman@outlook.com>
2023-04-23 20:48:09 -0400

    Fix tests

--- tests/Integration/Fixtures/User.php
+++ tests/Integration/Fixtures/User.php
@@ -2,16 +2,23 @@
 
 namespace LdapRecord\Tests\Integration\Fixtures;
 
+use LdapRecord\Models\Concerns\HasPassword;
 use LdapRecord\Models\OpenLDAP\Entry;
 use LdapRecord\Models\Relations\HasMany;
 
 class User extends Entry
 {
+    use HasPassword;
+
     public static array $objectClasses = [
         'top',
         'posixAccount',
         'inetOrgPerson',
     ];
+
+    protected string $passwordAttribute = 'userpassword';
+
+    protected string $passwordHashMethod = 'ssha';
 
     public function groups(): HasMany
     {
